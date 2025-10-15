e913245e62d6ff03d97247ac57a050f88996fbfb
dziwuj <zbyszko.sobecki@student.pk.edu.pl>
2025-10-15 15:26:11 +0200

    LAB03-FILES-4.3
    LAB03-FILES-FINISHED

--- Lab3_0/controllers/products.js
+++ Lab3_0/controllers/products.js
@@ -11,17 +11,30 @@ exports.getAddProduct = (req, res, next) => {
     });
 }
 
-exports.postAddProduct = (req, res, next) => {
-    const product = new Product(req.body.title);
-    product.save();
-    res.redirect('/');
-}
+exports.postAddProduct = async (req, res, next) => {
+    const title = req.body.title;
+    const image = req.file;
+    if(!image){
+        return res.status(422).render("add-product", {
+            pageTitle: "Add Product", path: "/admin/add-product",
+            formsCSS: true, productCSS: true,
+            activeAddProduct: true
+        });
+    }
+    const imageUrl = image.path;
+    const product = new Product(title, imageUrl);
+    await product.save();
+    res.redirect("/");
+};
 
-exports.getProducts = (req, res, next) => {
-    const products = Product.fetchAll();
+exports.getProducts = async (req, res, next) => {
+    const products = await Product.fetchAll();
     res.render('shop', {
         prods: products,
         pageTitle: 'Shop',
         path: '/',
+        hasProducts: products.length > 0,
+        activeShop: true, productCSS: true
     });
-}
+};
+
--- Lab3_0/models/product.js
+++ Lab3_0/models/product.js
@@ -1,15 +1,45 @@
+const path = require('path');
+const fs = require('fs').promises;
+
 const products = [];
 
+const fullName = path.join(
+    path.dirname(require.main.filename), "data", "products.json");
+
+const getProductsFromFile = async () => {
+    try{
+        const contents = await fs.readFile(fullName, 'utf8');
+        if(contents.length===0){ return []; }else{ return JSON.parse(contents); }
+    }catch(error){
+        //console.log(error);
+        return [];
+    }
+};
+
 module.exports = class Product{
-    constructor(t){
-        this.title = t;
+    constructor(t, img) {
+            this.title = t;
+            this.image = img;
+            this.price = Math.floor(Math.random() * 90) + 10;
+            this.desc = 'A very nice ' + this.title;
     }
-    save(){
-        this.price = Math.floor(Math.random() * 90) + 10;
-        this.desc = 'A very nice ' + this.title;
-        products.push(this);
+
+    async save() {
+        products.length = 0;
+        try{
+            const prods = await getProductsFromFile();
+            products.push(...prods);
+            products.push(this);
+            await fs.writeFile(fullName, JSON.stringify(products));
+        }catch(error){
+            console.log(error);
+        }
     }
-    static fetchAll(){
+
+    static async fetchAll() {
+        products.length = 0;
+        const prods = await getProductsFromFile();
+        products.push(...prods);
         return products;
     }
-}
+};
